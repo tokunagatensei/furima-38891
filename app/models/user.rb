3b68359f252626e7e7ff8cf9e21ef99e81380f6d
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-07 17:40:24 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,           presence: true
+  validates :encrypted_password, presence: true, format: { with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/}
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date,         presence: true
 end
