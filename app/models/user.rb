d895c2ee2ab0eb8dcb5de5ea4442faa1afb7642f
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-08 12:09:43 +0900

    ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,           presence: true
-  validates :encrypted_password, presence: true, format: { with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]{7,}/}
+  validates :encrypted_password, presence: true
   validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
