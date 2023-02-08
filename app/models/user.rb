d716a5a272e7f9b8c776249de2380e0e9a63e6bf
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-08 14:58:37 +0900

    ユーザー機能のバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,           presence: true
-  validates :encrypted_password, presence: true
-  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date,         presence: true
+  validates :nickname,              presence: true
+  validates :password,                              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date,            presence: true
 end
