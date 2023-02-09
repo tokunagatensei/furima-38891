c5cda21c30373a038a36e42989268a7eff436ad6
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-09 13:56:02 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   validates :nickname,              presence: true
   validates :password,                              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
