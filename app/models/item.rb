9ca1a035c080c14434b5c741e1edb4a080e9d91c
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-10 17:44:34 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+
+  validates :name,              presence: true
+  validates :description,       presence: true
+  validates :category_id,       numericality: { other_than: 1 }
+  validates :item_status_id,    numericality: { other_than: 1 }
+  validates :shipping_cost_id,  numericality: { other_than: 1 }
+  validates :prefecture_id,     numericality: { other_than: 1 }
+  validates :shipping_date_id,  numericality: { other_than: 1 }
+  validates :price,             presence: true, format: { with: /\A[0-9]+\z/ },
+                                numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
   belongs_to :user
   has_one_attached :image
 
