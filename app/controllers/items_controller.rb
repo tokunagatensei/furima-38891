a13f3648cf395ae7f7d8c67bdc75474653a408b6
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-12 13:57:50 +0900

    ログインの有無で条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
-    @items = Item.all
   end
 
   def new
@@ -9,6 +10,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
