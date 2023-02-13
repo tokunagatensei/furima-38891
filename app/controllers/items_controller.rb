4c1a5d696a49f5ab2b79b6b581780007dfdf2a9d
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-13 19:04:55 +0900

    コントローラーにedit,updateアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id == @item.user_id
+    
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
