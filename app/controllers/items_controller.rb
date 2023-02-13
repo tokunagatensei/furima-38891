8eb6015e6aecb72d72f87c543a9e9195cfdf964f
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-13 20:40:13 +0900

    editアクション内の記述を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,11 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id == @item.user_id
-    
-    else
-      redirect_to root_path
-    end
+    redirect_to root_path  if current_user.id != @item.user_id
   end
 
   def update
