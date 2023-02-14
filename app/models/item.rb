aed4cd91a542e45bce1b9e2b75047e79f23d0e1e
tokunagatensei <tensei.soccer14@icloud.com>
2023-02-14 15:27:10 +0900

    with_optionsで共通のバリデーションをグループ化

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
 
-  validates :image,             presence: true
-  validates :name,              presence: true
-  validates :description,       presence: true
-  validates :category_id,       numericality: { other_than: 1 }
-  validates :item_status_id,    numericality: { other_than: 1 }
-  validates :shipping_cost_id,  numericality: { other_than: 1 }
-  validates :prefecture_id,     numericality: { other_than: 1 }
-  validates :shipping_date_id,  numericality: { other_than: 1 }
-  validates :price,             presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price,         numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 
   belongs_to :user
   has_one_attached :image
